4d0e93f35ec616655ef9c5c4f9363603ce7a6d36
Sanjay Ranjanagi <152060588+sanjayranjanagi@users.noreply.github.com>
2024-07-07 10:13:34 +0530

    Create Dijkstras.cpp

---
+++ Codes/Dijkstras.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+class Graph {
+    int V;
+    vector<vector<pair<int, int>>> adj;
+
+public:
+    Graph(int V);
+    void addEdge(int u, int v, int w);
+    vector<int> dijkstra(int src);
+};
+
+Graph::Graph(int V) {
+    this->V = V;
+    adj.resize(V);
+}
+
+void Graph::addEdge(int u, int v, int w) {
+    adj[u].emplace_back(v, w);
+    adj[v].emplace_back(u, w); // Uncomment this for an undirected graph
+}
+
+vector<int> Graph::dijkstra(int src) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    vector<int> dist(V, INT_MAX);
+
+    pq.push(make_pair(0, src));
+    dist[src] = 0;
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        for (auto x : adj[u]) {
+            int v = x.first;
+            int weight = x.second;
+
+            if (dist[v] > dist[u] + weight) {
+                dist[v] = dist[u] + weight;
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+    return dist;
+}
+
+int main() {
+    int V = 9;
+    Graph g(V);
+
+    g.addEdge(0, 1, 4);
+    g.addEdge(0, 7, 8);
+    g.addEdge(1, 2, 8);
+    g.addEdge(1, 7, 11);
+    g.addEdge(2, 3, 7);
+    g.addEdge(2, 8, 2);
+    g.addEdge(2, 5, 4);
+    g.addEdge(3, 4, 9);
+    g.addEdge(3, 5, 14);
+    g.addEdge(4, 5, 10);
+    g.addEdge(5, 6, 2);
+    g.addEdge(6, 7, 1);
+    g.addEdge(6, 8, 6);
+    g.addEdge(7, 8, 7);
+
+    vector<int> dist = g.dijkstra(0);
+
+    cout << "Vertex Distance from Source\n";
+    for (int i = 0; i < V; ++i)
+        cout << i << "\t" << dist[i] << "\n";
+
+    return 0;
+}
